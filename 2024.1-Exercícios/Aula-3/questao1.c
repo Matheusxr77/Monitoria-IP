40de53abf06de15157dd6bd0e0dc632ca647e90e
Matheusxr77 <suehtamxr77@gmail.com>
2024-07-01 17:30:05 -0300

    Aula 03 - Variáveis, expressões lógicas e aritméticas

--- 2024.1-Exercícios/Aula-3/questao1.c
+++ 2024.1-Exercícios/Aula-3/questao1.c
@@ -1,6 +1,7 @@
 /* Questão 1°| Escreva um programa que solicita ao usuário que insira seu peso em quilogramas e sua altura em metros, e em seguida calcula e exibe o IMC. O IMC é calculado dividindo o peso pela altura ao quadrado.*/
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     float peso, altura, imc;
@@ -10,7 +11,7 @@ int main() {
     printf("Digite sua altura em metros: ");
     scanf("%f", &altura);
 
-    imc = peso / (altura * altura);
+    imc = peso / pow(altura, 2);
 
     printf("Seu IMC é: %.2f\n", imc);
 
